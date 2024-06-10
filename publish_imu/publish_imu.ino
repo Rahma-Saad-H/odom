27a0b3bd3abbce947ed06d14bc2cca67ab33623d
Rahma-Saad-H <saadrahma562@gmail.com>
2024-06-10 17:21:27 +0300

    Add files via upload

---
+++ publish_imu/publish_imu.ino
@@ -0,0 +1,81 @@
+#include <Wire.h>
+#include <ros.h>
+#include <std_msgs/Float32MultiArray.h>
+
+float RateRoll, RatePitch, RateYaw;
+float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
+int RateCalibrationNumber;
+
+ros::NodeHandle nh;
+
+std_msgs::Float32MultiArray imu_msg;
+
+ros::Publisher imu_pub("imu_data", &imu_msg);
+
+void gyro_signals() {
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1A);
+  Wire.write(0x05);
+  Wire.endTransmission(); 
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1B);
+  Wire.write(0x08);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43);
+  Wire.endTransmission(); 
+  Wire.requestFrom(0x68, 6);
+  int16_t GyroX = Wire.read() << 8 | Wire.read();
+  int16_t GyroY = Wire.read() << 8 | Wire.read();
+  int16_t GyroZ = Wire.read() << 8 | Wire.read();
+  RateRoll = (float)GyroX / 65.5;
+  RatePitch = (float)GyroY / 65.5;
+  RateYaw = (float)GyroZ / 65.5;
+}
+
+void setup() {
+  //Serial.begin(57600);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  Wire.setClock(400000);
+  Wire.begin();
+  delay(250);
+  Wire.beginTransmission(0x68); 
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  for (RateCalibrationNumber = 0; RateCalibrationNumber < 2000; RateCalibrationNumber++) {
+    gyro_signals();
+    RateCalibrationRoll += RateRoll;
+    RateCalibrationPitch += RatePitch;
+    RateCalibrationYaw += RateYaw;
+    delay(1);
+  }
+  RateCalibrationRoll /= 2000;
+  RateCalibrationPitch /= 2000;
+  RateCalibrationYaw /= 2000;
+
+  // Initialize ROS node handle and advertise publisher
+  nh.initNode();
+  nh.advertise(imu_pub);
+}
+
+void loop() {
+  gyro_signals();
+  RateRoll -= RateCalibrationRoll;
+  RatePitch -= RateCalibrationPitch;
+  RateYaw -= RateCalibrationYaw;
+
+  // Populate the data array with roll, pitch, and yaw values
+  float data[] = {RateRoll, RatePitch, RateYaw};
+  imu_msg.data_length = 3;
+  imu_msg.data = data;
+
+  imu_pub.publish(&imu_msg);
+
+  // Handle ROS communication
+  nh.spinOnce();
+  
+  // Delay to control publishing rate
+  delay(50);
+}
